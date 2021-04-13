8f614b779c9fec263881d5cffe63ab2a1b611939
verenazmo <verena.zmollnig@htl-salzburg.ac.at>
2021-04-13 09:22:50 +0200

    added Scene Switch feature

--- Assets/Scripts/QuestionManager.cs
+++ Assets/Scripts/QuestionManager.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text counterText;
+
+public void update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+     public void Scene2()
+    {
+        Application.LoadLevel(1);
+    }
+    
+    public void Scene3()
+    {
+        Application.LoadLevel(2);
+    }
+}
